05f5647ed6ff7fa95afb8bb946992afed41ea45e
martinizin <mar45pillacela@gmail.com>
2025-01-16 11:00:11 -0500

    Componentes iniciales

---
+++ src/componentes/actividades/actividades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actividades',
+  imports: [],
+  templateUrl: './actividades.component.html',
+  styleUrl: './actividades.component.css'
+})
+export class ActividadesComponent {
+
+}
